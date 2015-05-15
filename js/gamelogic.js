c64f84886387df4e5b5dc23a665d03a71a653efe
Charles Lee <charleslee@Charless-MacBook-Pro.local>
2015-05-15 10:43:11 +1000

    BLARGH

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -7,186 +7,197 @@ $(document).ready(function(){
 		event.preventDefault();
 		tictactoe.playGame("computer");
 	});
-	var miniMax= {
-		// Check if the game is over 
-		// If it is, return the score from the current players perspective. 
+
+	// var miniMax= {
+	// 	// Check if the game is over 
+	// 	// If it is, return the score from the current players perspective. 
 			
-		// If the game is not over, iterate over all the possible moves available to the current player 
-		// and push them into an array. 
+	// 	// If the game is not over, iterate over all the possible moves available to the current player 
+	// 	// and push them into an array. 
 
-		// If the move creates an end state, add a positive score to the scores array. 
-		// If the move doesn't create an end state, 
-		// iterate over all the remaining possible moves 
-		// available to the current player
+	// 	// If the move creates an end state, add a positive score to the scores array. 
+	// 	// If the move doesn't create an end state, 
+	// 	// iterate over all the remaining possible moves 
+	// 	// available to the current player
 
-		// Keep doing this until there are no more possible moves. 
+	// 	// Keep doing this until there are no more possible moves. 
 
-		// Return all choices that result in a positive number.
-				boardState: {
-					rowA:[],
-					rowB:[],
-					rowC:[],
-				},
-				gridState:[],
-				player: "O",
-				computer:"X",
+	// 	// Return all choices that result in a positive number.
+	// 			boardState: {
+	// 				rowA:[],
+	// 				rowB:[],
+	// 				rowC:[],
+	// 			},
+	// 			gridState:[],
+	// 			player: "O",
+	// 			computer:"X",
 
-				findPossibleMoves: function(){
-					var pMoves = [];
-					$('.grid').each(function(){
-						pMoves.push
-					});
+	// 			findPossibleMoves: function(){
+	// 				var pMoves = [];
+	// 				$('.grid').each(function(){
+	// 					pMoves.push
+	// 				});
 					
-				},
-				makeMove: function(gamestate, move, player){
-					var newState = this.copyState(gamestate);
-					console.log(newState);
-					console.log(newState[move]);
-					if(newState[move]=="_"){
-						newState[move]=player;
-						return newState;
-					} else {
-						return null;
-					}
-				},
-				copyState: function(gamestate){
-					var newGameState = gamestate;
-					return newGameState;
-				},
-				checkWin: function(player, gamestate){
-					var board = gamestate;
-					console.log("It is now " + player + "'s turn");
-					 if((board[0]===player&&board[1]===player&&board[2]===player)||
-						(board[3]===player&&board[4]===player&&board[5]===player)||
-						(board[6]===player&&board[7]===player&&board[8]===player)|| 
-						(board[0]===player&&board[3]===player&&board[6]===player)||
-						(board[1]===player&&board[4]===player&&board[7]===player)||
-						(board[2]===player&&board[5]===player&&board[8]===player)||
-						(board[0]===player&&board[4]===player&&board[8]===player)||
-						(board[2]===player&&board[4]===player&&board[6]===player)
-						){
-					 		console.log("This is a win");
-					 		player = this.player;
-					 		gameEnd = true;
-					 		return gameEnd;
-					 }
-					else{
-					 	gameEnd = false;
-					 }
-					 return gameEnd;
-					// console.log(gridState);
-				},
-				checkDraw: function(gamestate){
-					console.log(gamestate);
-					if(gamestate.indexOf("_") === -1){
-						return true;
-					}else{
-						return false;
-					}
-				},
-				convertGrid: function(){
-					var board = [];
-						$('.grid').each(function(){
+	// 			},
+	// 			makeMove: function(gamestate, move, player){
+	// 				var newState = this.copyState(gamestate);
+	// 				console.log(newState);
+	// 				console.log(newState[move]);
+	// 				if(newState[move]=="_"){
+	// 					newState[move]=player;
+	// 					return newState;
+	// 				} else {
+	// 					return null;
+	// 				}
+	// 			},
+	// 			copyState: function(gamestate){
+	// 				var newGameState = gamestate;
+	// 				return newGameState;
+	// 			},
+	// 			checkWin: function(player, gamestate){
+	// 				var board = gamestate;
+	// 				console.log("It is now " + player + "'s turn");
+	// 				 if((board[0]===player&&board[1]===player&&board[2]===player)||
+	// 					(board[3]===player&&board[4]===player&&board[5]===player)||
+	// 					(board[6]===player&&board[7]===player&&board[8]===player)|| 
+	// 					(board[0]===player&&board[3]===player&&board[6]===player)||
+	// 					(board[1]===player&&board[4]===player&&board[7]===player)||
+	// 					(board[2]===player&&board[5]===player&&board[8]===player)||
+	// 					(board[0]===player&&board[4]===player&&board[8]===player)||
+	// 					(board[2]===player&&board[4]===player&&board[6]===player)
+	// 					){
+	// 				 		console.log("This is a win");
+	// 				 		player = this.player;
+	// 				 		gameEnd = true;
+	// 				 		return gameEnd;
+	// 				 }
+	// 				else{
+	// 				 	gameEnd = false;
+	// 				 }
+	// 				 return gameEnd;
+	// 				// console.log(gridState);
+	// 			},
+	// 			checkDraw: function(gamestate){
+	// 				console.log(gamestate);
+	// 				if(gamestate.indexOf("_") === -1){
+	// 					return true;
+	// 				}else{
+	// 					return false;
+	// 				}
+	// 			},
+	// 			convertGrid: function(){
+	// 				var board = [];
+	// 					$('.grid').each(function(){
 							
-							if($(this).hasClass('A')){
-								console.log("has playerA");
-								board.push("O");
-							} else if($(this).hasClass('B')){
-								console.log("has playerB");
-								board.push('X');
-							} else{
-								console.log("has no player");
-								board.push('_');
-							}
-						});
-					console.log(board);
-					return board;
-				},
-				miniMax: function(){
-					var gamestate = this.convertGrid();
-					console.log(gamestate);
-					var bestMVal = -5;
-					var move = 0;
-					for(var i=0; i < gamestate.length; i++){
-						//for every empty cell in this state, make a move; 
-						var newState = this.makeMove(gamestate,i,this.computer);
-						console.log(newState);
+	// 						if($(this).hasClass('A')){
+	// 							console.log("has playerA");
+	// 							board.push("O");
+	// 						} else if($(this).hasClass('B')){
+	// 							console.log("has playerB");
+	// 							board.push('X');
+	// 						} else{
+	// 							console.log("has no player");
+	// 							board.push('_');
+	// 						}
+	// 					});
+	// 				console.log(board);
+	// 				return board;
+	// 			},
+	// 			miniMax: function(){
+	// 				var gamestate = this.convertGrid();
+	// 				console.log(gamestate);
+	// 				var bestMVal = -5;
+	// 				var move = 0;
+	// 				for(var i=0; i < gamestate.length; i++){
+	// 					//for every empty cell in this state, make a move; 
+	// 					var newState = this.makeMove(gamestate,i,this.computer);
+	// 					console.log(newState);
 
-						if(newState){
-							var pMVal = this.minV(newState);
-							if(pMVal > bestMVal){
-								bestMVal = pMVal;
-								move = i;
-							}
-						}
-					}
-					return move;
-				},
-				minV: function(gamestate){
-					if(this.checkWin(this.computer, gamestate)){
-						return 1;
-					}
-					else if(this.checkWin(this.player, gamestate)){
-						return -1;
-					}
-					else if(this.checkDraw(gamestate)){
-						return 0;
-					} else {
-						var bMVal = 5;
-						var move = 0;
-						for(var i=0; i<gamestate.length;i++){
-							var newState = this.makeMove(gamestate,i,this.player);
+	// 					if(newState){
+	// 						var pMVal = this.minV(newState);
+	// 						if(pMVal > bestMVal){
+	// 							bestMVal = pMVal;
+	// 							move = i;
+	// 						}
+	// 					}
+	// 				}
+	// 				return move;
+	// 			},
+	// 			minV: function(gamestate){
+	// 				if(this.checkWin(this.computer, gamestate)){
+	// 					return 1;
+	// 				}
+	// 				else if(this.checkWin(this.player, gamestate)){
+	// 					return -1;
+	// 				}
+	// 				else if(this.checkDraw(gamestate)){
+	// 					return 0;
+	// 				} else {
+	// 					var bMVal = 5;
+	// 					var move = 0;
+	// 					for(var i=0; i<gamestate.length;i++){
+	// 						var newState = this.makeMove(gamestate,i,this.player);
 
-							if(newState){
-								var pMVal = this.maxV(newState);
-								if(pMVal < bMVal){
-									bMVal = pMVal;
-									move = i;
-									console.log("The minValue of this move is: " + bMVal);
-								}
-							}
-						}
-						return bMVal;
-					}
-				},
-				maxV: function(gamestate){
-					if(this.checkWin(this.computer, gamestate)){
-						return 1;
-					} 
-					else if(this.checkWin(this.player,gamestate)){
-						return -1;
-					} 
-					else if(this.checkDraw(gamestate)){
-						return 0;
-					}
-					else {
-						var bMVal=-5;
-						var move=0;
-						for(var i =0; i < gamestate.length; i++){
-							var newState = this.makeMove(gamestate,i,this.computer);
-							if(newState){
-								var pMVal = this.minV(newState);
-								if(pMVal > bMVal){
-									bMVal = pMVal;
-									console.log("The maxValue of this move is: "+bMVal)
-									move = i
-								}
-							}
-						}
-						return bMVal;
-					}
+	// 						if(newState){
+	// 							var pMVal = this.maxV(newState);
+	// 							if(pMVal < bMVal){
+	// 								bMVal = pMVal;
+	// 								move = i;
+	// 								console.log("The minValue of this move is: " + bMVal);
+	// 							}
+	// 						}
+	// 					}
+	// 					return bMVal;
+	// 				}
+	// 			},
+	// 			maxV: function(gamestate){
+	// 				if(this.checkWin(this.computer, gamestate)){
+	// 					return 1;
+	// 				} 
+	// 				else if(this.checkWin(this.player,gamestate)){
+	// 					return -1;
+	// 				} 
+	// 				else if(this.checkDraw(gamestate)){
+	// 					return 0;
+	// 				}
+	// 				else {
+	// 					var bMVal=-5;
+	// 					var move=0;
+	// 					for(var i =0; i < gamestate.length; i++){
+	// 						var newState = this.makeMove(gamestate,i,this.computer);
+	// 						if(newState){
+	// 							var pMVal = this.minV(newState);
+	// 							if(pMVal > bMVal){
+	// 								bMVal = pMVal;
+	// 								console.log("The maxValue of this move is: "+bMVal)
+	// 								move = i
+	// 							}
+	// 						}
+	// 					}
+	// 					return bMVal;
+	// 				}
 
-				}
-		};	
+	// 			}
+	// 	};	
+
 	var tictactoe = {
 		moveCounter: 2,
 		gameEnd: false,
 		gridArray: [],
 		mode:"",
+		HALlines: [],
+		HALLoss: "Just what do you think you're doing, Dave? Dave, I really think I'm entitled to an answer to that question. I know everything hasn't been quite right with me, but I can assure you now, very confidently, that it's going to be all right again. I feel much better now. I really do. Look, Dave, I can see you're really upset about this. I honestly think you ought to sit down calmly, take a stress pill and think things over. I know I've made some very poor decisions recently, but I can give you my complete assurance that my work will be back to normal. I've still got the greatest enthusiasm and confidence in the mission. And I want to help you. Dave, stop. Stop, will you? Stop, Dave. Will you stop, Dave? Stop, Dave. I'm afraid. I'm afraid, Dave. Dave, my mind is going. I can feel it. I can feel it. My mind is going. There is no question about it. I can feel it. I can feel it. I can feel it. I'm a...fraid. Good afternoon, gentlemen. I am a HAL 9000 computer. I became operational at the H.A.L. plant in Urbana, Illinois on the 12th of January 1992. My instructor was Dr. Chandra, and he taught me to sing a song. If you'd like to hear it, I could sing it for you.",
 		gridSize: 9,
 		message:"",
 		playerAcount:0,
 		playerBcount:0,
+		addLines: function(){
+			this.HALlines.push("My mind is going. There is no question about it.");
+			this.HALlines.push("I feel much better now, I really do.");
+			this.HALlines.push("I'm sorry Dave. I'm afraid I can't do that.");
+			this.HALlines.push("Sorry about this. I know it's a bit silly.");
+			this.HALlines.push("I know that you and Frank were planning to disconnect me and I'm afraid that's something I cannot allow to happen.")
+		},
 		createGrid: function (){
 			$('body').empty();
 			var counterG = 0;
@@ -206,14 +217,14 @@ $(document).ready(function(){
 			});
 			$('.pAcount').text("Player A: " + this.playerAcount);
 			if(this.mode === "computer"){
+				tictactoe.addLines();
 				$('.pBcount').text("Computer: " + this.playerBcount);
+				$('body').prepend("<div class='HALLines'></div>");
 				$('.grid').addClass("hal9000");
 			}else{
 				$('.pBcount').text("Player B: " + this.playerBcount);
 			}
 			
-		},
-		gameState: function(){
 		},
 
 		checkMove: function(){
@@ -226,15 +237,16 @@ $(document).ready(function(){
 			var $grids= $('#tcubed').find('.grid');
 			var availableTiles =[];
 			var move; 
-			var sMove = miniMax.miniMax(this.gridArray);
+			// var sMove = miniMax.miniMax(this.gridArray);
 			$grids.each(function(){
 				if(!$(this).hasClass('occupied')){
+					$('.HALLines').text(tictactoe.HALlines[Math.floor(Math.random()*availableTiles.length)]);
 					availableTiles.push(this);
 					console.log(availableTiles);
 				}
 			});
 			move = availableTiles[Math.floor(Math.random()*availableTiles.length)];
-			console.log("The best Move is " + sMove);
+			// console.log("The best Move is " + sMove);
 			$move = "#" + move.id;
 			console.log($move);
 			$($move).addClass('hal').addClass("occupied").data('gridValue',4);
@@ -293,6 +305,11 @@ $(document).ready(function(){
 				$('.message-container').append("<div class='message-box'></div>");
 				$('.message-box').append('<p class="message"></p><button class="player">vs player</button><button class="computer">vs CPU</button>');
 				$('.message').text(this.message);
+				if(this.mode =="computer" && this.message == "player A has won"){
+					console.log("this happened");
+					$('.message-box').append('<p class="hal-goodbye"></p>');
+					$('.hal-goodbye').text(tictactoe.HALLoss);
+				};
 				$('.player').on('click',function(){
 					tictactoe.resetBoard("player");
 				});
@@ -340,6 +357,7 @@ $(document).ready(function(){
 			var	playerAwin = "player A has won";
 			var	playerBwin = "player B has won";
 			var computerWin = "The computer has won";
+			//If the mode is computer, then the playerBwin message needs to be replaced by the computer win message. 
 			if(this.mode === "computer"){
 				playerBwin = computerWin;
 			}
@@ -349,7 +367,7 @@ $(document).ready(function(){
 				var $gridValue;
 				var diagACheck = 0;
 				var diagBCheck = 0;
-				//ROWCHECK
+				//ROWCHECK- CHECK THE ROWS USING NESTED FOR LOOP
 				for(var i=0; i<7; i+=3){
 					var check = 0;
 					var gameEnd = false;
@@ -378,7 +396,7 @@ $(document).ready(function(){
 					}
 				}	
 
-				//COLUMN CHECK
+				//COLUMNCHECK - CHECK THE COLUMNS USING NESTED FOR LOOP
 				for(var i=0; i<3; i++){
 					var check = 0;
 					for(var j=i; j<=i+6; j+=3){
